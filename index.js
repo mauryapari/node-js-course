4d57a8f88ac1722ddfbe90947ad89a1b64ac84cf
Paritosh Maurya <paritosh.maurya@tadigital.com>
2022-05-18 19:05:38 +0530

    Express Router

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 
+
 const hostname = 'localhost';
 const port = 3000;
+
+const dishRouter = require('./routes/dishRouter');
 
 const app = express();
 
 app.use(morgan('dev'));
+
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
